a7888dbb4c2a4ea73305557ba05f076e976dca10
Khadijah Homolka <kkarrington21@gmail.com>
2023-06-29 16:45:08 -0700

    taking a subset

--- CTD Data/CTD Processing.R
+++ CTD Data/CTD Processing.R
@@ -1,17 +1,23 @@
 ## Loading packages
 require(pacman)
-p_load(tidyverse, oce, leaflet, sf)
+p_load(tidyverse, oce, leaflet, sf, devtools, driftR)
+
+# install.packages("devtools")
+#devtools::install_github("shaughnessyar/driftR")
 
 ## Importing data to work with 
-read.csv("/Users/khadijahhomolka/Documents/GitHub/PGST-Natural-Resources/CTD Data/KorEXO Measurement File Export - 022223 115854 (1).csv")
-
-data <- "exo_cast_022223"
-
-colnames(exo_cast_022223)[6] <- "cond_us_cm"
-colnames(exo_cast_022223)[7] <- "depth_m"
-
 ctd_data <- dr_read("./EXO_SD_13D101118_010820_183642.xlsx", "EXO", defineVar = TRUE, cleanVar = TRUE)
-?dr_read
+
+ctd_clean <- subset(ctd_data, depth_m > 0.5)
+
+ggplot(ctd_clean, aes(x=time_hh_mm_ss, y=depth_m)) +
+  geom_point() +
+  xlab("Time") + 
+  ylab("Depth (m)") + 
+  theme_minimal()
+
+
+
 
 
        
