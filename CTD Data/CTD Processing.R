bbdef94c4b9eba42639ede53603406ba46a11219
Khadijah Homolka <kkarrington21@gmail.com>
2023-06-29 15:57:20 -0700

    importing the data

--- CTD Data/CTD Processing.R
+++ CTD Data/CTD Processing.R
@@ -3,14 +3,15 @@ require(pacman)
 p_load(tidyverse, oce, leaflet, sf)
 
 ## Importing data to work with 
-read_xlsx("/Users/khadijahhomolka/Documents/R/exo_cast_022223.xlsx")
+read.csv("/Users/khadijahhomolka/Documents/GitHub/PGST-Natural-Resources/CTD Data/KorEXO Measurement File Export - 022223 115854 (1).csv")
 
 data <- "exo_cast_022223"
 
 colnames(exo_cast_022223)[6] <- "cond_us_cm"
 colnames(exo_cast_022223)[7] <- "depth_m"
 
-
+ctd_data <- dr_read("./EXO_SD_13D101118_010820_183642.xlsx", "EXO", defineVar = TRUE, cleanVar = TRUE)
+?dr_read
 
 
        
