efa55fe3d8e70d249172e47f4853390322881e3e
Khadijah Homolka <kkarrington21@gmail.com>
2023-11-08 23:03:40 -0800

    updated sample size to 10%

--- Hydroacoustics/Random Sampling Hydroacoustics.R
+++ Hydroacoustics/Random Sampling Hydroacoustics.R
@@ -30,17 +30,19 @@ TAST_OFF <- subset(TAST_files_clean, TAST_files_clean$`TAST status` == "OFF")
 ##ON <- 1147/6684 = 0.22
 ##OFF <- 5237/6684 = 0.78
 
-#determine sample size -> 335 files total seems reasonable to start with (5% of 6684)
+#determine sample size -> 669 files total seems reasonable to start with (10% of 6684)
 #calculate proportions with new sample size
-##ON <- 335*0.22 = 74
-##OFF <- 335*0.78 = 261
+##ON <- 669*0.22 = 147
+##OFF <- 669*0.78 = 522
 
 #random sampling within TAST_ON stratum
 set.seed(1994) #setting seed will ensure reproducibility of random generating (same numbers will be generated every time)
-sample_size <- 74 #based off a total sample size of 335 (5% of 6684), 74 is 22% of 335
+sample_size <- 147 #based off a total sample size n=669, 147 is 22% of 669
 random_TAST_ON <- TAST_ON[sample(nrow(TAST_ON), sample_size), , drop = FALSE]
+write.xlsx(random_TAST_ON, "Random Sampled TAST ON Files")
 
 #random sampling within TAST_OFF stratum
 set.seed(1994)
-sample_size <- 261 #based off a total sample size of 335 (5% of 6684), 261 is 78% of 335
+sample_size <- 522 #based off a total sample size n=669, 522 is 78% of 669
 random_TAST_OFF <- TAST_OFF[sample(nrow(TAST_ON), sample_size), , drop = FALSE]
+write.xlsx(random_TAST_OFF, "Random Sampled TAST OFF Files")
