c307f8fe4cf4e8612be367088acb6a6bbe62a5b9
kkhomolka <kkarrington21@gmail.com>
2023-11-28 09:24:05 -0800

    trying a two sample t-test

--- Hydroacoustics/Fillet/Power Analysis & Subsetting.R
+++ Hydroacoustics/Fillet/Power Analysis & Subsetting.R
@@ -1,4 +1,9 @@
 #load in packages
 pacman::p_load(pwr, ggplot2, tidyverse, dplyr)
 
-pwr.anova.test(k = 2, n = , f = 0.1, sig.level = 0.05, power = 0.9)
+#Determining the sample size (n) using a power analysis and the 'pwr' package 
+
+pwr.anova.test(k = 2, n = , f = 0.1, sig.level = 0.05, power = 0.9) #leaving n blank because that is the desired outcome 
+
+#trying the same thing with a two-sample t-test instead
+pwr.t.test(n =, d = 0.1, sig.level = 0.05, power = 0.9, type = c("two.sample"), alternative = "less")
