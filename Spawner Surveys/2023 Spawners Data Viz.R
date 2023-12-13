95db8cd1f983ccd242554e086dc93ed326d8dc6c
kkhomolka <kkarrington21@gmail.com>
2023-12-13 11:30:57 -0800

    started spawner survey script

---
+++ Spawner Surveys/2023 Spawners Data Viz.R
@@ -0,0 +1,14 @@
+
+## Load packages
+pacman::p_load(pwr, 
+               ggplot2, 
+               tidyr, 
+               dplyr,
+               cowsay,
+               lubridate,
+               tidyverse,
+               openxlsx)
+
+## Set working directory
+setwd("~/GitHub/PGST-Natural-Resources/Hydroacoustics/TAST")
+
