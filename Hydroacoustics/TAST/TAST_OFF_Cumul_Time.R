218cd7a1c99b91e66c810bbd768eb90c01580c91
Khadijah Homolka <kkarrington21@gmail.com>
2024-01-23 22:38:38 -0800

    updating plots

--- Hydroacoustics/TAST/TAST_OFF_Cumul_Time.R
+++ Hydroacoustics/TAST/TAST_OFF_Cumul_Time.R
@@ -11,10 +11,13 @@ pacman::p_load(pwr,
                openxlsx,
                readxl)
 
-## Set working directory
+## Set working directory for WORK
 setwd("~/GitHub/PGST-Natural-Resources/Hydroacoustics/TAST")
 
-## 2. Loading OFF Files --------------------------------------------------------
+##Set working directory for HOME 
+setwd("~/Documents/GitHub/PGST-Natural-Resources/Hydroacoustics/TAST")
+
+# 2. Loading OFF Files --------------------------------------------------------
 
 ## read in file and remove empty rows
 TAST_OFF_Times <- read_csv("Seal_presence_time_TAST_OFF_KH.csv")
@@ -46,7 +49,7 @@ TAST_ON_Times_clean$Date <- as.Date(TAST_ON_Times_clean$Date, format = "%m/%d/%Y
 #convert date from a continuous variable to a categorical
 TAST_ON_Times_clean$Date <- as.factor(TAST_ON_Times_clean$Date)
 
-#4. Check for duplicates or redundancies ---------------------------------------
+# 4. Check for duplicates or redundancies ---------------------------------------
 
 #check OFF
 duplicates_OFF <- TAST_OFF_Times_clean[duplicated(TAST_OFF_Times_clean[, c('File')]), ]
@@ -56,28 +59,45 @@ duplicates_ON <- TAST_ON_Times_clean[duplicated(TAST_ON_Times_clean[, c('File')]
 
 # 5. Combine dataframes and group by date --------------------------------------
 
+#bind both dataframes together into one 
 combined_ON_OFF <- rbind(TAST_OFF_Times_clean, TAST_ON_Times_clean)
 
-#convert date from a continuous variable to a categorical
-combined_ON_OFF$Date <- as.factor(combined_ON_OFF$Date)
-
 #removing 0 from the Cumulative_Time_s column
-combined_ON_OFF <- combined_ON_OFF %>% 
-  filter(Cumulative_Time_s != 0)
+#combined_ON_OFF <- combined_ON_OFF %>% 
+  #filter(Cumulative_Time_s != 0)
 
-#summarize to calculate sum of cumulative time
-summarized_df <- combined_ON_OFF %>%
+#summarize to calculate sum of cumulative time for ON
+summarized_ON <- TAST_ON_Times_clean %>%
   group_by(Date) %>%
   summarize(Cumulative_Time_sum = sum(Cumulative_Time_s))
 
-#brute force approach
-box_summarized_df <- combined_ON_OFF %>% 
+#summarize to calculate sum of cumulative time for OFF
+summarized_OFF <- TAST_OFF_Times_clean %>%
+  group_by(Date) %>%
+  summarize(Cumulative_Time_sum = sum(Cumulative_Time_s))
+
+#calculate stats for ON
+box_stats_ON <- TAST_ON_Times_clean %>% 
   group_by(Tast_Status) %>% 
   summarise(
     mean = mean(Cumulative_Time_s),
     sd = sd(Cumulative_Time_s),
+    se = sd(Cumulative_Time_s) / sqrt(n()),
     lower = mean(Cumulative_Time_s) - sd(Cumulative_Time_s),
     upper = mean(Cumulative_Time_s) + sd(Cumulative_Time_s))
+
+#calculate stats for OFF
+box_stats_OFF <- TAST_OFF_Times_clean %>% 
+  group_by(Tast_Status) %>% 
+  summarise(
+    mean = mean(Cumulative_Time_s),
+    sd = sd(Cumulative_Time_s),
+    se = sd(Cumulative_Time_s) / sqrt(n()),
+    lower = mean(Cumulative_Time_s) - sd(Cumulative_Time_s),
+    upper = mean(Cumulative_Time_s) + sd(Cumulative_Time_s))
+
+#binding the stats together for ON and OFF
+box_stats_combined <- rbind(box_stats_OFF, box_stats_ON)
 
 # 6. Draft Graphing ------------------------------------------------------------
 
@@ -110,6 +130,7 @@ ggplot(combined_ON_OFF, aes(x = Tast_Status, y = Cumulative_Time_s)) +
        y = "Cumulative Time (s)")+
   theme_minimal()
 
+
 # 7. Good Graphing -------------------------------------------------------------
 
 ## Plot a violin plot with customized boxplot points using different colors for "ON" and "OFF"
@@ -121,16 +142,47 @@ ggplot(combined_ON_OFF, aes(x = Tast_Status, y = Cumulative_Time_s, fill = Tast_
   labs(
     title = "Non-Normalized Cumulative Time of Seal Presence",
     x = "TAST Status",
-    y = "Cumulative Time (secs)"
-  ) +
-  theme_minimal() +
-  theme(legend.position = "none")
-
+    y = "Cumulative Time (secs)") +
+  theme_grey() +
+  theme(legend.position = "none")+
+  theme(plot.title = element_text(hjust = 0.5))
+  
+#boxplots 
 combined_ON_OFF %>%
   ggplot(aes(Date, Cumulative_Time_s, fill = Tast_Status))+
-  geom_boxplot()+
-  geom_jitter()+
+  geom_boxplot(width = 0.2, fill = "mediumpurple2", color = "black", outlier.shape = NA)+
+  geom_jitter(width = 0.2, size = 0.9, color = "black", alpha = 0.3)+
   facet_wrap(~Tast_Status)+
-  ggtitle("Non-normalized Cumulative Seal Presence")+
-  theme_minimal()
+  ggtitle("Non-Normalized Cumulative Time of Seal Presence")+
+  labs(x = "Date", y = "Cumulative Time (secs)")+
+  theme_grey()+
+    theme(legend.position = "none")+
+  theme(axis.text.x=element_text(angle=30, hjust=1))+
+  theme(plot.title = element_text(hjust = 0.5))
+
+# Create a boxplot from the summarized statistics
+ggplot(box_stats_combined, aes(x = Tast_Status, y = mean, fill = Tast_Status, group = Tast_Status)) +
+  geom_bar(stat = "identity", width = 0.6) +
+  geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2, position = position_dodge(width = 0.6)) +
+  scale_fill_manual(values = c("ON" = "mediumturquoise", "OFF" = "mediumpurple2")) +
+  labs(
+    title = "Bar Chart of Cumulative Time by TAST Status with Error Bars",
+    x = "TAST Status",
+    y = "Cumulative Time (s)")+
+  theme_classic()+
+  theme(legend.position = "none")+
+  theme(plot.title = element_text(hjust = 0.5))
+
+
+combined_ON_OFF %>%
+  mutate(Shortened_Date = format(Date, "%b %d")) %>%  # Create a new column with shortened dates
+  ggplot(aes(x = Shortened_Date, y = Cumulative_Time_s, fill = Tast_Status)) +
+  geom_boxplot(width = 0.2, fill = "mediumpurple2", color = "black", outlier.shape = NA) +
+  geom_jitter(width = 0.2, size = 0.9, color = "black", alpha = 0.3) +
+  facet_wrap(~Tast_Status) +
+  ggtitle("Non-Normalized Cumulative Time of Seal Presence") +
+  labs(x = "Date", y = "Cumulative Time (secs)") +
+  theme_grey() +
+  theme(legend.position = "none")
+
 
