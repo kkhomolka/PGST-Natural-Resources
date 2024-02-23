de5343caca3ea79c3fae8583c22fedfe6dae8e33
kkhomolka <kkarrington21@gmail.com>
2024-02-23 11:28:02 -0800

    combining echoview .csv exports into one file
    Created separate ON and OFF concatenated files for all the exports from echoview. Added a TAST_Status column manually in excel to make it easier to combine in R later

---
+++ Hydroacoustics/TAST/EchoView_files_concatenate.R
@@ -0,0 +1,46 @@
+# 1. Setup ---------------------------------------------------------------------
+
+## Load packages
+pacman::p_load(pwr, 
+               ggplot2, 
+               tidyr, 
+               dplyr,
+               cowsay,
+               lubridate,
+               tidyverse,
+               openxlsx,
+               readxl,
+               purrr)
+
+## Set working directory for KK WORK
+setwd("Z:/NR-Research/2023 Hood Canal Bridge/2023 TAST project/TAST Echoview Analysis/EV_Data_Export/EV_data_exports_Tast_ON")
+
+# 2. ON Files ---------------------------------------------------------------------
+
+files <- list.files(pattern = "*.csv")
+
+combined_data <- data.frame()
+
+for (file in files) {
+  data <- read.csv(file, header = TRUE)
+  combined_data <- rbind(combined_data, data)
+}
+
+write.csv(combined_data, "TAST_ON_EV_Export_combined.csv", row.names = FALSE)
+
+# 3. OFF Files -----------------------------------------------------------------
+
+#change the wd() to read all files in the OFF folder 
+setwd("Z:/NR-Research/2023 Hood Canal Bridge/2023 TAST project/TAST Echoview Analysis/EV_Data_Export/EV_data_exports_Tast_OFF")
+
+files <- list.files(pattern = "*.csv")
+
+combined_data <- data.frame()
+
+for (file in files) {
+  data <- read.csv(file, header = TRUE)
+  combined_data <- rbind(combined_data, data)
+}
+
+write.csv(combined_data, "TAST_OFF_EV_Export_combined.csv", row.names = FALSE)
+
