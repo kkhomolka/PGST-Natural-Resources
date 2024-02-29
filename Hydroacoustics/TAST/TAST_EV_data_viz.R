90fd43668985b1f635e941da97d607db301e62e3
Khadijah Homolka <kkarrington21@gmail.com>
2024-02-29 15:57:26 -0800

    Update TAST_EV_data_viz.R

--- Hydroacoustics/TAST/TAST_EV_data_viz.R
+++ Hydroacoustics/TAST/TAST_EV_data_viz.R
@@ -9,7 +9,12 @@ pacman::p_load(pwr,
                lubridate,
                tidyverse,
                openxlsx,
-               readxl)
+               readxl,
+               corrplot,
+               cowplot,
+               changepoint,
+               strucchange,
+               ggpubr)
 
 ## Set working directory for KK WORK
 setwd("~/GitHub/PGST-Natural-Resources/Hydroacoustics/TAST")
@@ -63,46 +68,62 @@ TAST_combined$DateTime_PST <- as.POSIXct(TAST_combined$DateTime_UTC, tz = "Ameri
 # remove date and just have Time_PST
 TAST_combined$Time_PST <- format(TAST_combined$DateTime_PST, format = "%H:%M:%S")
 
-# round time to the nearest hour
+# round time to the nearest hour and add :00 
 TAST_combined$DateTime_PST_rounded <- hour(TAST_combined$DateTime_PST)
-
-
+TAST_combined$DateTime_PST_rounded <- paste0(TAST_combined$DateTime_PST_rounded, ":00")
 
 # 5. Plotting ------------------------------------------------------------------
 
-# Time_in_beam vs. Foraging Time Window
+# Time_in_beam by Hour of Day
 TAST_combined %>% 
   ggplot(aes(DateTime_PST_rounded, Normalized_time_in_beam, color = TAST_Status))+
-  geom_jitter(aes(color = TAST_Status), size = 2.5, alpha = 0.4)+
+  geom_point(aes(color = TAST_Status), size = 2.5, alpha = 0.4)+
   ggtitle("Normalized Seal Time in Beam by Peak Foraging Time Window")+
   scale_color_manual(values = c("ON" = "darkgreen", "OFF" = "darkred")) +
-  labs(x = "Foraging Time (24 hrs)", y = "Normalized Seal Time in Beam (s)")+
+  labs(x = "Hour of Day", y = "Normalized Seal Time in Beam (s)")+
   theme_classic()+
   theme(plot.title = element_text(hjust = 0.5))+
   scale_x_continuous(breaks = seq(min(TAST_combined$DateTime_PST_rounded), 
                                   max(TAST_combined$DateTime_PST_rounded)))
+
+# Time_in_beam by DateTime
+TAST_combined %>% 
+  ggplot(aes(DateTime_PST, Normalized_time_in_beam, color = TAST_Status))+
+  geom_point(aes(color = TAST_Status), size = 2.5, alpha = 0.4)+
+  ggtitle("Normalized Seal Time in Beam by Peak Foraging Time Window")+
+  scale_color_manual(values = c("ON" = "darkgreen", "OFF" = "darkred")) +
+  labs(x = "Hour of Day", y = "Normalized Seal Time in Beam (s)")+
+  theme_classic()+
+  theme(plot.title = element_text(hjust = 0.5))+
+  scale_x_continuous(breaks = seq(min(TAST_combined$DateTime_PST_rounded), 
+                                  max(TAST_combined$DateTime_PST_rounded)))
+
   
 # Time_in_beam density plot
 TAST_combined %>% 
   ggplot(aes(Time_in_beam, color = TAST_Status))+
   geom_density()+
   ggtitle("Normalized Seal Time in Beam")
+  
+# 6. Correlation Plots ---------------------------------------------------------
 
-# Time_in_beam vs. TAST_Status
-TAST_combined %>% 
-  ggplot(aes(TAST_Status, Normalized_time_in_beam))
+# assigning factors and isolating numeric values only 
+TAST_cor <- TAST_combined %>% 
+  mutate(Status_numeric = case_when(TAST_Status == "ON" ~ 1,
+                                    TAST_Status == "OFF" ~ 2))
 
-  
-# Time_in_beam  
-TAST_combined %>% 
-  ggplot(aes(Time_in_beam, color = TAST_Status, fill = TAST_Status))+
-  geom_dotplot()+
-  ggtitle("Normalized Seal Time in Beam")
-  
-  
-  
-  
-  
+TAST_cor <- TAST_combined %>% 
+  select(where(is.numeric)) %>% 
+  drop_na() %>% 
+  select(-DateTime_PST_rounded, -Process_ID, -Target_depth_mean)
 
+# basic correlation function, using spearman method for categorical variables with assigned factors 
+cor(TAST_cor, method = "spearman")
+  
+pal <- wes_palette("Darjeeling2", 21, type = "continuous")
+
+matrix <- cor(TAST_cor) %>% 
+  corrplot(addCoef.col = "black", col = COL2("BrBG"), tl.srt = 45, tl.col = "black",
+           type = "lower", shade.col = c("blue", "tan"))
 
 
