f907e16326568afd63410bc68d286ecbb2e5fd9b
kkhomolka <kkarrington21@gmail.com>
2024-11-12 11:46:34 -0800

    Fixed the overlapping point issue with using facets. I'm happy with how they look now. Also cleaned up the code.

--- Snorkel Surveys/Snorkel_Survey_Script.R
+++ Snorkel Surveys/Snorkel_Survey_Script.R
@@ -19,9 +19,6 @@ pacman::p_load(pwr,
                ggformula,
                reshape2)
 
-# Aesthetics color palette
-pal <- wes_palette("AsteroidCity1", 2, type = "continuous")
-
 ## Set working directory for KK WORK
 setwd("~/GitHub/PGST-Natural-Resources/Snorkel Surveys")
 
@@ -90,9 +87,11 @@ all_habitats <- snorkel %>%
   filter(Habitat_Type %in% c("High Density Kelp", "Low Density Kelp", 
                              "Sandy Area", "Rip Rap"))
 
-# FISH
+# 5. Reshaping Data ------------------------------------------------------------
+
+# Salmon pivot longer
 salmon <- snorkel %>% 
-  select("Date","Chum", "Coho", "Pink", "Cutthroat", "Chinook", 
+  select("Habitat_Type","Date","Chum", "Coho", "Pink", "Cutthroat", "Chinook", 
          "Unknown_Salmon") %>% 
   mutate(across(Chum:Unknown_Salmon, ~ ifelse(. == "Yes", 1, 0)))
 
@@ -101,8 +100,9 @@ salmon_long <- salmon %>%
                names_to = "Juvenile_Salmon_Species",
                values_to = "Presence")
 
+# Forage fish pivot longer
 forage_fish <- snorkel %>% 
-  select("Date", "Herring", "Sand_Lance", "Surf_Smelt", 
+  select("Habitat_Type", "Date", "Herring", "Sand_Lance", "Surf_Smelt", 
          "Three_Spine_Stickleback","Anchovy", "Unknown_Forage_Fish") %>% 
   mutate(across(Herring:Unknown_Forage_Fish, ~ ifelse(. == "Yes", 1, 0)))
 
@@ -111,13 +111,14 @@ forage_fish_long <- forage_fish %>%
                names_to = "Forage_Fish_Species",
                values_to = "Presence")
 
+# Bind both dfs
 fish_long_combined <- bind_rows(salmon_long %>% mutate(
   Type = "Juvenile_Salmon_Species"),
   forage_fish_long %>% mutate(Type = "Forage_Fish_Species"))
 
-## 5. Plotting -----------------------------------------------------------------
+## 6. Plotting -----------------------------------------------------------------
 
-# Facet panels for Habitat Type vs. Overall Kelp Health Metric
+# Facet panels for Habitat Type vs. Overall Kelp Health
 ggplot(all_habitats, aes(x = Date, y = Overall_Kelp_Health, color = Habitat_Type)) +
   geom_line() +  
   labs(x = "Date", y = "Overall Kelp Health", title = "Kelp Health Over 2024 Growing Season") +
@@ -135,25 +136,57 @@ ggplot(all_habitats, aes(x = Date, y = Bryozoan_Epibiont, color = Habitat_Type))
   theme_bw()+
   facet_wrap(~ Habitat_Type, scales = "free_y")
 
+# Facet panel for Habitat Type vs. Sorus
+ggplot(all_habitats, aes(x = Date, y = Sorus, color = Habitat_Type)) +
+  geom_line() +  
+  labs(x = "Date", y = "Sorus Development", title = "Sorus Development Over 2024 Growing Season") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b")+
+  scale_y_continuous(limits = c(1,5), breaks = 1:5)+
+  theme_bw()+
+  facet_wrap(~ Habitat_Type, scales = "free_y")
 
-## 6. Heat Map -----------------------------------------------------------------
+# Facet panel for Habitat Type vs. Kelp Crab
+ggplot(all_habitats, aes(x = Date, y = Kelp_Crab_Epifauna, color = Habitat_Type)) +
+  geom_line() +  
+  labs(x = "Date", y = "Kelp Crab Coverage", title = "Kelp Crab Coverage Over 2024 Growing Season") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b")+
+  scale_y_continuous(limits = c(1,5), breaks = 1:5)+
+  theme_bw()+
+  facet_wrap(~ Habitat_Type, scales = "free_y")
 
-# Create a dot matrix for the salmon_long dataframe
-ggplot(salmon_long, aes(x = Date, y = Juvenile_Salmon_Species)) +
-  geom_point(aes(color = as.factor(Presence)), size = 3) +  # Add points for presence/absence
-  scale_color_manual(values = c("grey", "blue"), labels = c("Absent", "Present"), name = "Presence") +  # Color for presence/absence
-  labs(x = "Date", y = "Salmon Species", title = "Presence of Juvenile Salmon Over Time") +  # Labels for the plot
-  theme_minimal() +  # Clean theme
-  theme(axis.text.x = element_text(angle = 45, hjust = 1))  # Rotate x-axis labels for readability
+# Facet panel for Habitat Type vs. Filamentous Algae
+ggplot(all_habitats, aes(x = Date, y = Filamentous_Epiphyte, color = Habitat_Type)) +
+  geom_line() +  
+  labs(x = "Date", y = "Filamentous Epiphytic Algae Growth", title = "Filamentous Epiphytic Algal Growth Over 2024 Growing Season") +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b")+
+  scale_y_continuous(limits = c(1,5), breaks = 1:5)+
+  theme_bw()+
+  facet_wrap(~ Habitat_Type, scales = "free_y")
 
+# Missing rows from geom_line()?
+# Rows are removed from geom)line() because the df contains NAs, which is OK
+
+# Look for NA values in the relevant columns
+sum(is.na(all_habitats$Overall_Kelp_Health))
+sum(is.na(all_habitats$Date))
+
+# Or see which rows have NA values
+missing_rows <- all_habitats[is.na(all_habitats$Overall_Kelp_Health) | is.na(all_habitats$Date), ]
+print(missing_rows)
+
+## 6. Dot Matrices -------------------------------------------------------------
+
+# Dot Matrix for Forage Fish with Points Jittered
 ggplot(forage_fish_long, aes(x = Date, y = Forage_Fish_Species)) +
-  geom_point(aes(color = as.factor(Presence)), size = 3) +  # Add points for presence/absence
+  geom_point(aes(color = as.factor(Presence)), size = 3,
+             position = position_jitter(width = 0, height = 0.5)) +  # Add points for presence/absence
   scale_color_manual(values = c("grey", "blue"), labels = c("Absent", "Present"), name = "Presence") +  # Color for presence/absence
   labs(x = "Date", y = "Forage Fish Species", title = "Presence of Forage Fish Over Time") +
   scale_x_date(date_breaks = "1 week", date_labels = "%b/%d")+
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
+# Dot Matrix for Juvenile Salmon with Points Jittered
   ggplot(salmon_long, aes(x = Date, y = Juvenile_Salmon_Species)) +
     geom_point(aes(color = as.factor(Presence)), 
                size = 3,
@@ -167,6 +200,23 @@ ggplot(forage_fish_long, aes(x = Date, y = Forage_Fish_Species)) +
          title = "Presence of Juvenile Salmon Over Time") +
     theme_minimal() +
     theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  
+  # Salmon and Habitat Type Facet
+  ggplot(salmon_long, aes(x = Date, y = Juvenile_Salmon_Species, color = as.factor(Presence))) +
+    geom_point(size = 3, alpha = 0.7) +
+    scale_color_manual(values = c("grey", "navy"), labels = c("Absent", "Present"), name = "Presence") +
+    scale_x_date(date_breaks = "2 week", date_labels = "%b %d")+
+    facet_wrap(~ Habitat_Type, ncol = 2) +
+    labs(x = "Date", y = "Salmon Species", title = "Juvenile Salmon Presence During 2024 Field Season") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
 
-
-
+  # Forage Fish and Habitat Facet
+  ggplot(forage_fish_long, aes(x = Date, y = Forage_Fish_Species, color = as.factor(Presence))) +
+    geom_point(size = 3, alpha = 0.7) +
+    scale_color_manual(values = c("grey", "navy"), labels = c("Absent", "Present"), name = "Presence") +
+    scale_x_date(date_breaks = "2 week", date_labels = "%b/%d")+
+    facet_wrap(~ Habitat_Type, ncol = 2) +
+    labs(x = "Date", y = "Forage Fish Species", title = "Forage Fish Presence During 2024 Field Season") +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 45, hjust = 1))
