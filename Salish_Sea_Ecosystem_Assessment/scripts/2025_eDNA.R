81956a7b724c07a61ce2472606383a42116f8469
Khadijah Homolka <kkarrington21@gmail.com>
2026-05-29 13:04:02 -0700

    organizing an getting the new project setup

---
+++ Salish_Sea_Ecosystem_Assessment/scripts/2025_eDNA.R
@@ -0,0 +1,28 @@
+# 1. Setup ---------------------------------------------------------------------
+
+## Load packages
+pacman::p_load(pwr, 
+               ggplot2, 
+               tidyr, 
+               dplyr,
+               cowsay,
+               lubridate,
+               tidyverse,
+               openxlsx,
+               readxl,
+               corrplot,
+               cowplot,
+               changepoint,
+               strucchange,
+               ggpubr,
+               stats,
+               ggfortify,
+               vegan,
+               wesanderson,
+               ggrepel,
+               ggformula,
+               showtext,
+               ggeffects)
+
+font_add("Times New Roman", "/Library/Fonts/Times New Roman.ttf")
+showtext_auto()
